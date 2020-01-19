47409f45bee03b67e074e6b0b2564d1b8fa65003
Daniel Stephenson <dmccoystephenson@gmail.com>
2020-01-18 22:59:14 -0700

    Added cars going different speeds, no collision
    Made them look like they're coming from off the screen too!

--- frogHopper.cpp
+++ frogHopper.cpp
@@ -38,12 +38,35 @@ class Frog {
 	SDL_Rect collider;
 };
 
+
+class Vehicle {
+  public:
+	Vehicle();
+	void render(string direction);
+	void init(int x, int y, int w, int h, int xspeed, int xinitial);
+	void move();
+	int xpos;
+	int ypos;
+	int width;
+	int height;
+	int xvel;
+	int initialX;
+	SDL_Rect collider;
+	
+};
+
 // textures
 SDL_Texture* background;
 SDL_Texture* frogTexture;
+SDL_Texture* carRightTexture;
+SDL_Texture* carLeftTexture;
 
 // objects
 Frog frog;
+Vehicle bottomCarRight;
+Vehicle topCarRight;
+Vehicle bottomCarLeft;
+Vehicle topCarLeft;
 
 int frogX = 475;
 int frogY = 675;
@@ -57,8 +80,12 @@ void init() {
 	gWindow = SDL_CreateWindow("Frog Hopper - Frogger Clone", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 	gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 	
-	// initialize and render frog
+	// initialize objects
 	frog.init(frogX, frogY, frogW, frogH);	
+	bottomCarRight.init(-200, 550, 200, 100, 6, -200);
+	topCarRight.init(500, 200, 200, 100, 7, -200);
+	bottomCarLeft.init(400, 450, 200, 100, -8, SCREEN_WIDTH);
+	topCarLeft.init(200, 100, 200, 100, -9, SCREEN_WIDTH);
 	
 	//  initialize PNG loading
 	int imgFlags = IMG_INIT_PNG;
@@ -72,6 +99,12 @@ void loadMedia() {
 	temp_surface = IMG_Load("frog.png");
 	SDL_SetColorKey(temp_surface, SDL_TRUE, SDL_MapRGB(temp_surface->format, 0, 0xFF, 0xFF));
 	frogTexture = SDL_CreateTextureFromSurface(gRenderer, temp_surface);
+	temp_surface = IMG_Load("carRight.png");
+	SDL_SetColorKey(temp_surface, SDL_TRUE, SDL_MapRGB(temp_surface->format, 0, 0xFF, 0xFF));
+	carRightTexture = SDL_CreateTextureFromSurface(gRenderer, temp_surface);
+	temp_surface = IMG_Load("carLeft.png");
+	SDL_SetColorKey(temp_surface, SDL_TRUE, SDL_MapRGB(temp_surface->format, 0, 0xFF, 0xFF));
+	carLeftTexture = SDL_CreateTextureFromSurface(gRenderer, temp_surface);
 	SDL_FreeSurface(temp_surface);
 }
 
@@ -81,14 +114,6 @@ void cleanUp() {
 	IMG_Quit();
 	SDL_Quit();
 }
-/*
-class Vehicle {
-  public:
-	
-  private:
-	
-}
-*/
 
 Frog::Frog() {
 	xpos = 0;
@@ -170,9 +195,52 @@ void Frog::move() {
 	}
 }
 
+Vehicle::Vehicle() {
+	xpos = 0;
+	ypos = 0;
+	width = 0;
+	height = 0;
+}
+
+void Vehicle::render(string direction) {
+	SDL_Rect renderQuad = {xpos, ypos, width, height};
+	if (direction == "right") {
+		SDL_RenderCopy(gRenderer, carRightTexture, NULL, &renderQuad);
+	}
+	else if (direction == "left") {
+		SDL_RenderCopy(gRenderer, carLeftTexture, NULL, &renderQuad);
+	}
+}
+
+void Vehicle::init(int x, int y, int w, int h, int xspeed, int xinitial) {
+	xpos = x;
+	ypos = y;
+	width = w;
+	height = h;
+	collider = {x, y, w, h};
+	xvel = xspeed;
+	initialX = xinitial;
+}
+
+void Vehicle::move() {
+	xpos += xvel;
+	collider.x = xpos;
+	
+	// if too far left or right
+	if ((xpos < -200) || (xpos + width > SCREEN_WIDTH + 200)) {
+		// move to initial x
+		xpos = initialX;
+		collider.x = xpos;
+	}
+}
+
 void renderScene() {	
 	SDL_RenderCopy(gRenderer, background, NULL, NULL);
 	frog.render();
+	bottomCarRight.render("right");
+	topCarRight.render("right");
+	bottomCarLeft.render("left");
+	topCarLeft.render("left");
 }
 
 bool checkWin() {
@@ -185,8 +253,11 @@ bool checkWin() {
 void start() {
 	renderScene();
 	frog.move();
+	bottomCarRight.move();
+	topCarRight.move();
+	bottomCarLeft.move();
+	topCarLeft.move();
 	checkWin();
-	frog.render();
 }
 
 int main(int argc, char* args[]) {
